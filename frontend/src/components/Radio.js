3020656730510604ff678b11ad76734c30f4d6e3
Ramon Wijnands <ramon@ruvu.nl>
2020-02-03 11:53:40 +0100

    Refactor AddSongModal

--- frontend/src/components/Radio.js
+++ frontend/src/components/Radio.js
@@ -2,72 +2,23 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Media from 'react-bootstrap/Media';
 import { FaPlus, FaPlay } from 'react-icons/fa';
 
-import { useRadio, search } from './hooks';
+import { useRadio } from './hooks';
+import SearchModal from './SearchModal';
 
-function AddButton() {
+function AddSongButton() {
   const [show, setShow] = useState(false);
-  const [results, setResults] = useState([]);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  function onSearch(e) {
-    e.preventDefault();
-
-    const { q } = Object.fromEntries(new FormData(e.target.form));
-    search(q).then(videos => {
-      setResults(videos);
-    });
-  }
 
   return (
     <>
       <Button onClick={handleShow}>
         <FaPlus /> Add a song
       </Button>
-
-      <Modal show={show} onHide={handleClose} size="lg">
-        <Modal.Header closeButton>
-          <Modal.Title>Add a song</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <InputGroup>
-              <Form.Control type="search" name="q" placeholder="Search..." />
-              <InputGroup.Append>
-                <Button onClick={onSearch}>Search</Button>
-              </InputGroup.Append>
-            </InputGroup>
-          </Form>
-
-          {results.map(song => (
-            <Media>
-              <img
-                width={64}
-                height={64}
-                className="mr-3"
-                src="holder.js/64x64"
-                alt="Generic placeholder"
-              />
-              <Media.Body>
-                <h5>{song.name}</h5>
-                <p>20k views</p>
-              </Media.Body>
-            </Media>
-          ))}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cancel
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <SearchModal show={show} onClose={handleClose} onSubmit={handleClose} />
     </>
   );
 }
@@ -85,7 +36,7 @@ function Radio({ radioId }) {
       <Button variant="success">
         <FaPlay /> Play
       </Button>
-      <AddButton />
+      <AddSongButton />
       <pre>{JSON.stringify(radio, null, 2)}</pre>
       {/* TODO: list of songs */}
     </>
---
+++ frontend/src/components/SearchModal.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Media from 'react-bootstrap/Media';
+import { FaPlus, FaPlay } from 'react-icons/fa';
+
+import { search } from './hooks';
+
+export default function SearchModal({ show, onClose }) {
+  const [results, setResults] = useState([]);
+
+  function onSearch(e) {
+    e.preventDefault();
+
+    const { q } = Object.fromEntries(new FormData(e.target.form));
+    search(q).then(videos => {
+      setResults(videos);
+    });
+  }
+
+  return (
+    <>
+      <Modal show={show} onHide={onClose} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Add a song</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <InputGroup>
+              <Form.Control type="search" name="q" placeholder="Search..." />
+              <InputGroup.Append>
+                <Button onClick={onSearch}>Search</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
+
+          <div className="mt-3">
+            {results.map(song => (
+              <Media key={song.id}>
+                <img
+                  width={64}
+                  height={64}
+                  className="mr-3"
+                  src="holder.js/64x64"
+                  alt="Generic placeholder"
+                />
+                <Media.Body>
+                  <h5>{song.name}</h5>
+                  <p>20k views</p>
+                </Media.Body>
+                <Button variant="success">
+                  <FaPlus />
+                </Button>
+              </Media>
+            ))}
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onClose}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
