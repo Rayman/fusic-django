6d9865f6657aac38030c9c94ba53a74f29480d15
Ramon Wijnands <ramon@ruvu.nl>
2020-02-06 22:19:54 +0100

    Add HTTP 503 when youtube is unavailabe

--- backend/youtube.py
+++ backend/youtube.py
@@ -1,18 +1,24 @@
 import googleapiclient.discovery
 import googleapiclient.errors
 from django.conf import settings
+from httplib2 import ServerNotFoundError
+from rest_framework.exceptions import APIException
 
-api_service_name = "youtube"
-api_version = "v3"
+
+class YoutubeServiceUnavailable(APIException):
+    status_code = 503
+    default_detail = "Youtube API service temporarily unavailable, try again later."
+    default_code = "service_unavailable"
+
 
 youtube = googleapiclient.discovery.build(
-    api_service_name,
-    api_version,
-    developerKey=settings.YOUTUBE_API_KEY,
-    cache_discovery=False,
+    "youtube", "v3", developerKey=settings.YOUTUBE_API_KEY, cache_discovery=False,
 )
 
 
 def search(q):
     request = youtube.search().list(part="snippet", q="the doors", type="video")
-    return request.execute()
+    try:
+        return request.execute()
+    except ServerNotFoundError as e:
+        raise YoutubeServiceUnavailable() from e
