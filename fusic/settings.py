abf78da7328a7a0d7f317108e2f2687c963d7240
Ramon Wijnands <rayman747@hotmail.com>
2020-03-31 22:18:51 +0200

    Add rest_auth for authentication

--- fusic/settings.py
+++ fusic/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_extensions",
     "rest_framework",
+    "rest_auth",
 ]
 
 MIDDLEWARE = [
--- fusic/urls.py
+++ fusic/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("api/", include("backend.urls")),
+    path("api/auth/", include("rest_auth.urls")),
     path("admin/", admin.site.urls),
 ]
