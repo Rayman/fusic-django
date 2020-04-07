e5bc17cbec5a3d4d0f316160d97536cb2add7cef
Ramon Wijnands <rayman747@hotmail.com>
2020-04-07 22:33:47 +0200

    Scaffold a login modal

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,37 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { FaUser } from 'react-icons/fa';
+import { FaLock } from 'react-icons/fa';
 
 import NoSSR from '../components/NoSSR';
 import { useAuthState } from './hooks';
 
 import './NavBar.css';
 
+function LoginModal({ show, onClose }) {
+  return (
+    <Modal show={show} onHide={onClose} size="sm">
+      <Modal.Header closeButton>
+        <Modal.Title className="ml-auto">Member login</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text id="inputGroupPrepend">
+                  <FaUser />
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control type="email" placeholder="Email" />
+            </InputGroup>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text>
+                  <FaLock />
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control type="password" placeholder="Password" />
+            </InputGroup>
+          </Form.Group>
+
+          <Button variant="success" block>
+            Login
+          </Button>
+        </Form>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="link" block size="sm">
+          Forgot password?
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function LoginButton() {
+  const [show, setShow] = useState(false);
+
+  function login() {
+    setShow(true);
+  }
+
+  function handleClose() {
+    setShow(false);
+  }
+
+  return (
+    <>
+      <Button className="float-right" variant="link" onClick={login}>
+        Login
+      </Button>
+      <LoginModal show={show} onClose={handleClose} />
+    </>
+  );
+}
+
+function LogoutButton() {
+  function logout() {
+    // TODO: logout
+  }
+
+  return (
+    <Button className="float-right" variant="link" onClick={logout}>
+      Logout
+    </Button>
+  );
+}
+
 function LoginLogoutButton() {
   const [user, loading, error] = useAuthState();
 
   if (error) throw error;
-
-  function login() {
-    // var provider = new firebase.auth.GoogleAuthProvider();
-    // firebase.auth().signInWithRedirect(provider);
-  }
-
-  function logout() {
-    // firebase.auth().signOut();
-  }
-
   if (loading) return null;
-  else {
-    return user ? (
-      <Button className="float-right" variant="link" onClick={logout}>
-        Logout
-      </Button>
-    ) : (
-      <Button className="float-right" variant="link" onClick={login}>
-        Login
-      </Button>
-    );
-  }
+  return user ? <LogoutButton /> : <LoginButton />;
 }
 
 export default function NavBar() {
